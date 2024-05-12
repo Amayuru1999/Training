dc7f88397cb4c03bc84503aaf3b837bc478dbced
Amayuru1999 <amarasingheau@gmail.com>
2024-05-12 13:28:42 +0530

    Initial

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,3 +1,22 @@
 public class PalindromeChecker {
+        public static boolean isPalindrome(String str){
+            str=str.replaceAll("\\s","").toLowerCase();
 
+            int length=str.length();
+
+            if(length <= 1){
+                return true;
+            }
+            for(int i=0;i<length/2;i++){
+                if(str.charAt(i) != str.charAt(length-i-1)){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    public static void main(String[] args) {
+        String input="Amayuru";
+        System.out.println(isPalindrome(input));
+    }
 }
